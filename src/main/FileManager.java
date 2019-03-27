70bd82d4a8b2fafe7b894ed5e59b7d1dc2436c1b
Juan David Solano <juansolanosilva@gmail.com>
2019-03-27 13:52:39 -0500

    Started fileManager

--- src/main/FileManager.java
+++ src/main/FileManager.java
@@ -1,14 +1,38 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package main;
 
-/**
- *
- * @author juans
- */
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 public class FileManager {
-    
+
+    // Create File
+    public void createFile(String name, String extension) throws IOException {
+        if (extension == null) {
+            extension = ".txt";
+        }
+        // Create a file
+        File f;
+        f = new File(name+extension);
+
+        // Create necesary objects
+        FileWriter fr = new FileWriter(f, true); // True value means dont will erase file's previous content
+        BufferedWriter bw = new BufferedWriter(fr);
+        PrintWriter pw = new PrintWriter(bw);
+
+        // Write
+        pw.print("Same ");
+        pw.print("line!!!");
+        bw.newLine();
+        pw.print("Other Line!!!");
+        pw.println("Hi there!");
+
+        // Close
+        pw.close();
+        bw.close();
+        fr.close();
+    }
+
 }
